6e319b5a5d2a9684a01934084527c89dd2cdce71
Himanshu <108611598+himanshu-niet@users.noreply.github.com>
2024-01-10 08:46:12 +0000

    kk

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const next = require('next')
 
 const port = 5000
-const dev = process.env.NODE_ENV !== 'production'
+const dev = true
 
 // creating the app either in production or dev mode 
 const app = next({ dev })
